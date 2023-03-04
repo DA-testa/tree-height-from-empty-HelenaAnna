52266ac1855e374154682cace0f6f79047e364ed
HelenaAnnaBunka <125254498+HelenaAnna@users.noreply.github.com>
2023-03-04 15:30:00 +0000

    test3

--- tree_height.py
+++ tree_height.py
@@ -3,6 +3,7 @@ import sys
 import threading
 
 def compute_height(n, parents):
+    # Write this function
     children = {i: [] for i in range(n)}
 
     # populate the children dictionary
@@ -21,22 +22,33 @@ def compute_height(n, parents):
     return height(root)
 
 def main():
-    if len(sys.argv) > 1:
-        # read input from file
-        filename = sys.argv[1]
-        if 'a' in filename:
-            print("Invalid filename. File name cannot contain letter 'a'.")
-            return
-        with open(filename, 'r') as f:
-            n = int(f.readline().strip())
-            parents = list(map(int, f.readline().strip().lstrip().split()))
-    else:
-        # read input from keyboard
-        n = int(input())
-        parents = list(map(int, input().strip().lstrip().split()))
+    # implement input form keyboard and from files
+    
+    # let user input file name to use, don't allow file names with letter a
+    # account for github input inprecision
+    
+    # input number of elements
+    # input values in one variable, separate with space, split these values in an array
+    # call the function and output it's result
+    try:
+        if len(sys.argv) > 1:
+            # read input from file
+            filename = sys.argv[1]
+            if 'a' in filename:
+                print("Invalid filename. File name cannot contain letter 'a'.")
+                return
+            with open(filename, 'r') as f:
+                n = int(f.readline().strip())
+                parents = list(map(int, f.readline().strip().split()))
+        else:
+            # read input from keyboard
+            n = int(input())
+            parents = list(map(int, input().strip().split()))
 
-    max_height = compute_height(n, parents)
-    print(max_height)
+        max_height = compute_height(n, parents)
+        print(max_height)
+    except ValueError:
+        print("Invalid input. Please enter numeric values only.")
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
