cf96d1e7457c412b81f99e02350c3916fd03a422
HelenaAnnaBunka <125254498+HelenaAnna@users.noreply.github.com>
2023-03-04 15:27:04 +0000

    test2

--- tree_height.py
+++ tree_height.py
@@ -1,12 +1,9 @@
 # python3
-
 import sys
 import threading
-import numpy as np
 
 def compute_height(n, parents):
-    # Write this function
-     children = {i: [] for i in range(n)}
+    children = {i: [] for i in range(n)}
 
     # populate the children dictionary
     for i, p in enumerate(parents):
@@ -24,15 +21,7 @@ def compute_height(n, parents):
     return height(root)
 
 def main():
-    # implement input form keyboard and from files
-    
-    # let user input file name to use, don't allow file names with letter a
-    # account for github input inprecision
-    
-    # input number of elements
-    # input values in one variable, separate with space, split these values in an array
-    # call the function and output it's result
-     if len(sys.argv) > 1:
+    if len(sys.argv) > 1:
         # read input from file
         filename = sys.argv[1]
         if 'a' in filename:
@@ -55,5 +44,3 @@ def main():
 sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
-main()
-# print(array([1,2,3]))
