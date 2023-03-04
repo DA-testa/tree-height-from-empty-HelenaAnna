68fd7e1177a8b57fc02590f6436d1d512eac9f29
HelenaAnnaBunka <125254498+HelenaAnna@users.noreply.github.com>
2023-03-04 16:07:52 +0000

    las

--- tree_height.py
+++ tree_height.py
@@ -55,7 +55,7 @@ def main():
     max_height = compute_height(n, values)
     print(max_height)
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
     sys.setrecursionlimit(10 ** 7)
     threading.stack_size(2 ** 27)
     threading.Thread(target=main).start()
